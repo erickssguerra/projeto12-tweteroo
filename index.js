ea263554f3728e3e21d0b29ff53d42b2c8141183
erickssguerra <erickssguerra@gmail.com>
2022-11-06 18:46:31 -0300

    Versão Final 2.1 - Bônus Carregar páginas de 10 em 10 tweets

--- index.js
+++ index.js
@@ -54,14 +54,91 @@ const tweets = [
 		tweet: "esses meus alunos um dia vão me matar"
 	},
 	{
-		username: "holandesvoador",
+		username: "10",
+		avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVE2omP_cEI-j4dYmcNqvwdKBreIM5NSwTACkJ0HnvdFQIo-Gml7dGIUJOif-7SOtJFBI&usqp=CAU",
+		tweet: "o sinistro holandes voador"
+	},
+	{
+		username: "11",
+		avatar: "https://pm1.narvii.com/6350/dda5a981d12aeb5ecb8c70105194dd7f27b973c9_hq.jpg",
+		tweet: "esses meus alunos um dia vão me matar"
+	},
+	{
+		username: "12",
+		avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVE2omP_cEI-j4dYmcNqvwdKBreIM5NSwTACkJ0HnvdFQIo-Gml7dGIUJOif-7SOtJFBI&usqp=CAU",
+		tweet: "o sinistro holandes voador"
+	},
+	{
+		username: "13",
+		avatar: "https://s.gravatar.com/avatar/e08dfbb9cbab6ec22af90299c400036b?s=200",
+		tweet: "olárrr"
+	},
+	{
+		username: "14",
+		avatar: "https://upload.wikimedia.org/wikipedia/pt/b/b1/Patrick_Estrela.png",
+		tweet: "eu odeio o hub"
+	},
+	{
+		username: "15",
+		avatar: "https://assets.papelpop.com/wp-content/uploads/2021/05/sandy-bob-esponja.png",
+		tweet: "quem ta afim de caçar agua-vivas?"
+	},
+	{
+		username: "16",
+		avatar: "https://cf.shopee.com.br/file/c5f7cf93461037798997592a698e7fea",
+		tweet: "eu quero tocar flauta e reclamar da vida"
+	},
+	{
+		username: "17",
+		avatar: "https://i.pinimg.com/550x/c4/8b/1b/c48b1bedcef71689757ef0e0f19fcfdd.jpg",
+		tweet: "eu amo dinheiro"
+	},
+	{
+		username: "18",
+		avatar: "http://pm1.narvii.com/6738/0d700e090c890c04e2a14019cb356609d190c68av2_00.jpg",
+		tweet: "alguem sabe a formula do hamburguer de siri?"
+	},
+	{
+		username: "19",
+		avatar: "https://t.ctcdn.com.br/HUV06WevURQNovPIV7H0It4q79c=/700x394/smart/filters:format(webp)/i445916.jpeg",
+		tweet: "eu amo fazer exercicios"
+	},
+	{
+		username: "20",
+		avatar: "http://pm1.narvii.com/6382/43062c5065d0e1fc4065047e44ddd8708bec6d8f_00.jpg",
+		tweet: "eu amo ver o larry fazer exercicio <3"
+	},
+	{
+		username: "21",
+		avatar: "https://pm1.narvii.com/6350/dda5a981d12aeb5ecb8c70105194dd7f27b973c9_hq.jpg",
+		tweet: "esses meus alunos um dia vão me matar"
+	},
+	{
+		username: "22",
+		avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVE2omP_cEI-j4dYmcNqvwdKBreIM5NSwTACkJ0HnvdFQIo-Gml7dGIUJOif-7SOtJFBI&usqp=CAU",
+		tweet: "o sinistro holandes voador"
+	},
+	{
+		username: "23",
+		avatar: "https://pm1.narvii.com/6350/dda5a981d12aeb5ecb8c70105194dd7f27b973c9_hq.jpg",
+		tweet: "esses meus alunos um dia vão me matar"
+	},
+	{
+		username: "24",
 		avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVE2omP_cEI-j4dYmcNqvwdKBreIM5NSwTACkJ0HnvdFQIo-Gml7dGIUJOif-7SOtJFBI&usqp=CAU",
 		tweet: "o sinistro holandes voador"
 	},
 ]
 
+// só funciona para o front-end 2 (Bônus)
 server.get("/tweets", (req, res) => {
-	res.send(tweets)
+
+	const page = req.query.page
+	let sortedTweets = tweets;
+	for (let i = 0; i < page; i++) {
+		sortedTweets = tweets.slice(i * 10, page * 10)
+	}
+	res.status(201).send(sortedTweets)
 })
 
 server.get("/tweets/:username", (req, res) => {
